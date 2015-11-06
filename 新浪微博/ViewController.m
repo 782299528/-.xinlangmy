ee068420c3fa5527878412c7da9c58b062b08c91
782299528 <13693125668@163.com>
2015-11-06 18:21:37 +0800

    检测冲突

--- 新浪微博/ViewController.m
+++ 新浪微博/ViewController.m
@@ -16,8 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-//    github测试
+// Do any additional setup after loading the view, typically from a nib.
+//    github
 }
 
 - (void)didReceiveMemoryWarning {
